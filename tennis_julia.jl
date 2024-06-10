2bad6745141dadedc353ea7f7c0441352203642f
wongl01 <90303797+wongl01@users.noreply.github.com>
2024-06-10 10:05:28 -0700

    working version for constant sd

--- tennis_julia.jl
+++ tennis_julia.jl
@@ -8,31 +8,19 @@ using Distributions
 using Random
 using StatsFuns
 using LogDensityProblems
+using BenchmarkTools
+using Profile
+using PProf
 
-include("./data/fetch_data.jl")
 
-data = create_arrays()
-winner_ids = get(data,"winner_ids",nothing)
-loser_ids = get(data,"loser_ids",nothing)
-player_encoder = get(data,"player_encoder",nothing)
 
 #stan_file = "./model/model.stan"
 
-#stan_data = [
-#    length(winner_ids),
-#    length(player_encoder.classes_),
-#    winner_ids .+ 1,
-#    loser_ids .+ 1]
 
-stan_data = [
-    10,
-    4,
-    [1,3,1,4,1,3,2,3,4,4],
-    [2,4,3,2,4,2,1,1,1,1]]
 
 #json_data = JSON.json(stan_data)
 
-print("pigeons start")
+#print("pigeons start")
 
 invlogit(z::Real) = 1/(1+exp(-clamp(z,-709,709)))
 
@@ -65,38 +53,62 @@ function (log_potential::MyLogPotential)(params)
     for i in 1:log_potential.n_players
         lp -= player_skills[i]^2
     end
-    return -(log_likelihood + player_sd*lp)
+    return (log_likelihood + player_sd*lp)
 end
 function Pigeons.initialization(log_potential::MyLogPotential, rng::AbstractRNG, _::Int64) 
     player_skills_raw = randn(rng, log_potential.n_players)
     player_sd = 1#abs(randn(rng, Float64))
-    print(player_sd)
-    params = push!(player_skills_raw, player_sd)
+    #print(player_sd)
+    #params = push!(player_skills_raw, player_sd)
     
-    return params
+    return player_skills_raw#params
 end
 function Pigeons.sample_iid!(log_potential::MyLogPotential, replica, shared)
     rng = replica.rng
-    new_state = push!(randn(rng, log_potential.n_players), 1)#abs(randn(rng, Float64)))
+    #new_state = push!(randn(rng, log_potential.n_players), 1)#abs(randn(rng, Float64)))
+    new_state = randn!(rng, replica.state)
     replica.state = new_state
 end
 
 
-n_matches = stan_data[1]
-n_players = stan_data[2]
-winner_ids = stan_data[3]
-loser_ids = stan_data[4]
+#LogDensityProblems.dimension(lp::MyLogPotential) = n_players
+#LogDensityProblems.logdensity(lp::MyLogPotential,x) = lp(x)
 
-LogDensityProblems.dimension(lp::MyLogPotential) = n_players
-LogDensityProblems.logdensity(lp::MyLogPotential,x) = lp(x)
 
-log_potential = MyLogPotential(n_matches, n_players, winner_ids, loser_ids)
-pt = pigeons(target=log_potential, reference = MyLogPotential(0,4,[1,1,1,1],[2,2,2,2]),
-    record=[traces;record_default()])
-report(pt)
 #pt = pigeons(target = StanLogPotential(stan_file,json_data), 
 #   record = [traces;round_trip;record_default()], 
  #   explorer = SliceSampler(w=5))
 
 
 #report(pt)
+
+function main()
+    #include("./data/fetch_data.jl")
+
+    #data = create_arrays()
+    #winner_ids = get(data,"winner_ids",nothing)
+    #loser_ids = get(data,"loser_ids",nothing)
+    #player_encoder = get(data,"player_encoder",nothing)
+
+    #stan_data = [
+    #length(winner_ids),
+    #length(player_encoder.classes_),
+    #winner_ids .+ 1,
+    #loser_ids .+ 1]
+
+    stan_data = [
+    17,
+    4,
+    [1,2,1,1,3,1,4,4,2,2,3,2,4,4,3,3,4],
+    [2,1,2,3,1,4,1,1,3,3,2,4,2,2,4,4,3]]
+
+    n_matches = stan_data[1]
+    n_players = stan_data[2]
+    winner_ids = stan_data[3]
+    loser_ids = stan_data[4]
+
+    log_potential = MyLogPotential(n_matches, n_players, winner_ids, loser_ids)
+    pt = @time pigeons(target=log_potential, reference = MyLogPotential(0,4,[1,1,1,1],[2,2,2,2]),
+    record=[traces;record_default()])
+    #report(pt)
+end
